f5b10cca7619bd5bdaa127108a43c992651cd2cc
rupali21 <rupali21.mitech@gmail.com>
2023-12-22 17:50:08 +0530

    student dashboard changes

--- app/Views/StudentSidebar/StudentSelectClassDates.php
+++ app/Views/StudentSidebar/StudentSelectClassDates.php
@@ -16,58 +16,68 @@
                     </ol>
                 </div>
             </div>
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
+                        <!-- <div class="card-header">
+                            <h3 class="card-title">Select Faculty Slots From This</h3>
+                        </div> -->
+                        <!-- <div class="card-body"> -->
+                            
                         <div class="card-header">
-                            <h3 class="card-title">Selelect Faculty Slots From This</h3>
+                            <?php if ($assignFacultyData === null) { ?>
+                                <h3 class="card-title">You have already selected a class Time</h3>
+                            <?php }else if($Assign_Techer_id =='') { ?>
+                                <h3 class="card-title">No faculty assigned yet! Please wait untill admin asign faculty to you.</h3>
+                            <?php }else { ?>
+                                <h3 class="card-title">Select Faculty Slots From This</h3>
+                            <?php }?>
                         </div>
+                        <?php if ($Assign_Techer_id) { ?>
                         <div class="card-body">
-    <?php if ($assignFacultyData['assignFacultyData'] === null) : ?>
-        <p>You have already selected a class Time .</p>
-    <?php else : ?>
-        <form id="facultyForm" action="<?= base_url('selectedslotsfromstudent'); ?>" method="post">
-            <!-- Hidden input for registerId -->
-            <input type="hidden" name="registerId" value="<?= $registerId; ?>">
+                            <form id="facultyForm" action="<?= base_url('selectedslotsfromstudent'); ?>" method="post">
+                                <!-- Hidden input for registerId -->
+                                <input type="hidden" name="registerId" value="<?= $registerId; ?>">
 
-            <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Faculty Name</th>
-                        <th>Date</th>
-                        <th>Start Time</th>
-                        <th>End Time</th>
-                        <th>Select</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if (!empty($assignFacultyData['assignFacultyData'])) : ?>
-                        <?php foreach ($assignFacultyData['assignFacultyData'] as $facultyData) : ?>
-                            <tr>
-                                <td><?= $facultyData->full_name; ?></td>
-                                <td><?= $facultyData->date; ?></td>
-                                <td><?= $facultyData->start_time; ?></td>
-                                <td><?= $facultyData->end_time; ?></td>
-                                <td>
-                                    <!-- Submit button with the id value -->
-                                    <button type="submit" name="selectedId" value="<?= $facultyData->id; ?>" class="btn btn-block btn-success">Select</button>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php else : ?>
-                        <tr>
-                            <td colspan="5">No available classes.</td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
-        </form>
-    <?php endif; ?>
-</div>
+                                <table id="example2" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Faculty Name</th>
+                                            <th>Date</th>
+                                            <th>Start Time</th>
+                                            <th>End Time</th>
+                                            <th>Select</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php if (!empty($assignFacultyData['assignFacultyData'])) : ?>
+                                            <?php foreach ($assignFacultyData['assignFacultyData'] as $facultyData) : ?>
+                                                <tr>
+                                                    <td><?= $facultyData->full_name; ?></td>
+                                                    <td><?= $facultyData->date; ?></td>
+                                                    <td><?= $facultyData->start_time; ?></td>
+                                                    <td><?= $facultyData->end_time; ?></td>
+                                                    <td>
+                                                        <!-- Submit button with the id value -->
+                                                        <button type="submit" name="selectedId" value="<?= $facultyData->id; ?>" class="btn btn-block btn-success">Select</button>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        <?php else : ?>
+                                            <tr>
+                                                <td colspan="5">No available classes.</td>
+                                            </tr>
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
+                            </form>
+                        </div>
+                        <?php } ?>
+                        <!-- </div> -->
 
                     </div>
                 </div>
@@ -76,4 +86,4 @@
         </div>
     </section>
 </div>
-<?php echo view('FacultysideBar/FacultyFooter.php'); ?>
+<?php echo view('StudentSidebar/StudentFooter.php'); ?>
