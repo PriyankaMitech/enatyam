49f5260a2be16ed42f5582eb6b5a75b8a42e81a2
PriyankaMitech <priyankag.mitech@gmail.com>
2024-01-09 10:19:15 +0530

    Changes For selects Slots in student Dashboard

--- app/Views/StudentSidebar/StudentSelectClassDates.php
+++ app/Views/StudentSidebar/StudentSelectClassDates.php
@@ -1,28 +1,25 @@
 <?php echo view('StudentSidebar/Studentsidebar'); ?>
 
-
-
 <link rel="stylesheet" href="public/calendar/fullcalendar/lib/main.min.css">
 <script src="public/calendar/js/jquery-3.6.0.min.js"></script>
 <script src="public/calendar/fullcalendar/lib/main.min.js"></script>
-   
+
 <div class="content-wrapper">
     <section class="content-header">
         <div class="container-fluid">
-
-        <div class="container py-5" id="page-container">
-    <div class="row">
-        <div class="col-md-9">
-            <div id="calendar"></div>
-        </div>
-        <div class="col-md-3">
+            <div class="container py-5" id="page-container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div id="calendar"></div>
+                    </div>
+                    <div class="col-md-3">
                         <div class="cardt rounded-0 shadow">
                             <div class="card-header bg-gradient bg-primary text-light">
-                                <h5 class="card-title">Select Schedule </h5>
+                                <h5 class="card-title">Select Schedule</h5>
                             </div>
                             <div class="card-body">
                                 <div class="container-fluid">
-                                    <form action="<?php echo base_url()?>gy" method="post"
+                                    <form action="<?php echo base_url()?>selectslotsbystudent" method="post"
                                         id="schedule-form">
                                         <input type="hidden" name="id" value="">
                                         <div class="form-group mb-2" id="select_day">
@@ -52,18 +49,20 @@
                                                 <div class="col-md-12">
                                                     <div class="form-group">
                                                         <label>Sub Courses</label>
-                                                        <input type="hidden" id="selected_shedules_time" value="<?php if (isset($single_data)) { echo ($single_data->shedules_time); } ?>">
-                                                        <select name="shedules_time" id="shedules_time" class="form-control">
+                                                        <input type="hidden" id="selected_shedules_time"
+                                                            value="<?php if (isset($single_data)) { echo ($single_data->shedules_time); } ?>">
+                                                        <select name="shedules_time" id="shedules_time"
+                                                            class="form-control">
                                                             <option value="">Please select sub courses</option>
                                                         </select>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                      
-                                        
-                                        <button class="btn btn-primary btn-sm rounded-0" type="submit"
-                                            form="schedule-form"><i class="fa fa-save"></i>View</button>
+                                        <button class="btn btn-primary btn-sm rounded-0" type="submit" form="schedule-form"
+        value="<?php echo isset($_SESSION['id']) ? $_SESSION['id'] : ''; ?>_<?php echo isset($_SESSION['Assign_Techer_id']) ? $_SESSION['Assign_Techer_id'] : ''; ?>">
+    <i class="fa fa-save"></i> Submit
+</button>
                                         <button class="btn btn-default border btn-sm rounded-0" type="reset"
                                             form="schedule-form"><i class="fa fa-reset"></i> Cancel</button>
                                     </form>
@@ -75,45 +74,48 @@
                             </div>
                         </div>
                     </div>
-    </div>
-</div>
-    <!-- Event Details Modal -->
-    <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content rounded-0">
-                <div class="modal-header rounded-0">
-                    <h5 class="modal-title">Schedule Details</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body rounded-0">
-                    <div class="container-fluid">
-                        <dl>
-                            <dt class="text-muted">Title</dt>
-                            <dd id="title" class="fw-bold fs-4"></dd>
-                            <dt class="text-muted">Description</dt>
-                           <a> <dd id="description" class=""></dd></a>
-                            <dt class="text-muted">Start</dt>
-                            <dd id="start" class=""></dd>
-                            <dt class="text-muted">End</dt>
-                            <dd id="end" class=""></dd>
-                        </dl>
-                    </div>
-                </div>
-                <div class="modal-footer rounded-0">
-                    <div class="text-end">
-                        <button type="button" class="btn btn-primary btn-sm rounded-0" id="edit" data-id="">Edit</button>
-                        <button type="button" class="btn btn-danger btn-sm rounded-0" id="delete" data-id="">Delete</button>
-                        <button type="button" class="btn btn-secondary btn-sm rounded-0" data-bs-dismiss="modal">Close</button>
+            </div>
+            <!-- Event Details Modal -->
+            <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content rounded-0">
+                        <div class="modal-header rounded-0">
+                            <h5 class="modal-title">Schedule Details</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body rounded-0">
+                            <div class="container-fluid">
+                                <dl>
+                                    <dt class="text-muted">Title</dt>
+                                    <dd id="title" class="fw-bold fs-4"></dd>
+                                    <dt class="text-muted">Description</dt>
+                                    <a>
+                                        <dd id="description" class=""></dd>
+                                    </a>
+                                    <dt class="text-muted">Start</dt>
+                                    <dd id="start" class=""></dd>
+                                    <dt class="text-muted">End</dt>
+                                    <dd id="end" class=""></dd>
+                                </dl>
+                            </div>
+                        </div>
+                        <div class="modal-footer rounded-0">
+                            <div class="text-end">
+                                <button type="button" class="btn btn-primary btn-sm rounded-0" id="edit"
+                                    data-id="">Edit</button>
+                                <button type="button" class="btn btn-danger btn-sm rounded-0" id="delete"
+                                    data-id="">Delete</button>
+                                <button type="button" class="btn btn-secondary btn-sm rounded-0"
+                                    data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-        </div>
     </section>
 </div>
-
-
 
 <?php echo view('StudentSidebar/StudentFooter.php'); ?>
 <?php 
@@ -129,7 +131,7 @@ foreach($data as $data){
 }
 ?>
 <script>
-    var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
+var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
 </script>
 <script src="public/calendar/js/script.js"></script>
 <script>
@@ -139,43 +141,26 @@ document.addEventListener("DOMContentLoaded", function() {
     function toggleDayOptions() {
         var dayOptionsDiv = document.getElementById("dayOptions");
         if (optionTypeRadio[0].checked) {
-            dayOptionsDiv.innerHTML = '<label class="control-label">Select Day(s)</label>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Monday">' +
-                '<label class="form-check-label">Monday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Tuesday">' +
-                '<label class="form-check-label">Tuesday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Wednesday">' +
-                '<label class="form-check-label">Wednesday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Thursday">' +
-                '<label class="form-check-label">Thursday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Friday">' +
-                '<label class="form-check-label">Friday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Saturday">' +
-                '<label class="form-check-label">Saturday</label>' +
-                '</div>' +
-                '<div class="form-check">' +
-                '<input type="checkbox" class="form-check-input" name="days[]" value="Sunday">' +
-                '<label class="form-check-label">Sunday</label>' +
+            <?php if (!empty($day_wise_shedules)) : ?>
+            dayOptionsDiv.innerHTML = '<label class="control-label">Select Day(s)</label>';
+            <?php foreach ($day_wise_shedules as $data) : ?>
+            dayOptionsDiv.innerHTML += '<div class="form-check">' +
+                '<input type="checkbox" class="form-check-input" name="days[]" value="<?php echo $data->Daystype; ?>" <?php echo ($data->Daystype !== $data->Daystype) ? 'disabled' : ''; ?>>' +
+                '<label class="form-check-label"><?php echo $data->Daystype; ?></label>' +
                 '</div>';
+            <?php endforeach; ?>
+            <?php else : ?>
+            dayOptionsDiv.innerHTML = '';
+            <?php endif; ?>
         } else {
-
             dayOptionsDiv.innerHTML = '';
         }
     }
-    for (var i = 0; i < optionTypeRadio.length; i++) {
+
+    for (var i = 0; optionTypeRadio && i < optionTypeRadio.length; i++) {
         optionTypeRadio[i].addEventListener("change", toggleDayOptions);
     }
+
     toggleDayOptions();
 });
 
@@ -196,51 +181,156 @@ document.addEventListener("DOMContentLoaded", function() {
         return `${year}-${month}-${day}T${hours}:${minutes}`;
     }
 });
-</script>
 
-
-<script>
-     $(document).ready(function () {
+$(document).ready(function() {
     var selectedDays = [];
+    var selectedOptionType = $('input[name="option_type"]:checked').val();
+    var addedTimeSlots = [];
 
-
-
-    $('input[name="days[]"]').on('change', function () {
+    $('input[name="days[]"]').on('change', function() {
         selectedDays = [];
+        selectedOptionType = $('input[name="option_type"]:checked').val();
 
-        selectedOptionType = '';
-
-        $('input[name="days[]"]:checked').each(function () {
+        $('input[name="days[]"]:checked').each(function() {
             selectedDays.push($(this).val());
-
-
         });
-      
-        $.ajax({ 
+
+        fetchData(selectedDays, selectedOptionType);
+    });
+
+    $('input[name="option_type"]').on('change', function() {
+        selectedOptionType = $(this).val();
+
+        if (selectedOptionType == 'allDay') {
+            $('input[name="days[]"]').prop('checked', false);
+            $('#shedules_time').empty();
+            resetDropdownAndFetchData();
+        } else {
+            $('#shedules_time').empty();
+        }
+    });
+
+    function resetDropdownAndFetchData() {
+        $('#shedules_time').empty();
+        $('#shedules_time').append('<option value="">Please select time</option>');
+        addedTimeSlots = []; // Reset added time slots
+        fetchData(selectedDays, selectedOptionType);
+    }
+
+    function createOneHourSlots(start, end) {
+        var slots = [];
+        var currentSlot = new Date(start);
+
+        while (currentSlot < end) {
+            var formattedStart = formatTo12Hour(currentSlot);
+            currentSlot.setHours(currentSlot.getHours() + 1);
+            var formattedEnd = formatTo12Hour(currentSlot);
+            var slot = formattedStart + ' - ' + formattedEnd;
+            slots.push(slot);
+        }
+
+        return slots;
+    }
+
+    function formatTo12Hour(time) {
+        var hours = time.getHours();
+        var period = hours >= 12 ? 'PM' : 'AM';
+        hours = (hours % 12) || 12;
+        var minutes = time.getMinutes().toString().padStart(2, '0');
+        return hours + ':' + minutes + ' ' + period;
+    }
+
+    function fetchData(selectedDays, selectedOptionType) {
+        if (selectedOptionType == 'allDay') {
+            selectedDays = '';
+        }
+
+        $.ajax({
             url: '<?= base_url(); ?>get_shedule_data_for_student',
             type: 'POST',
             data: {
                 selectedDays: selectedDays,
-                selectedOptionType : selectedOptionType
+                selectedOptionType: selectedOptionType
             },
             dataType: 'json',
-            success: function (data) {
-                $('#shedules_time').empty();
-                $('#shedules_time').append('<option value="">Please select sub Courses</option>');
-                $.each(data, function (key, value) {
-                    $('#shedules_time').append('<option value="' + value.id + '">' + value.start_datetime + '</option>');
+            success: function(data) {
+                $.each(data, function(key, value) {
+                    var startDatetime = new Date(value.start_datetime);
+                    var endDatetime = new Date(value.end_datetime);
+
+                    var slots = createOneHourSlots(startDatetime, endDatetime);
+
+                    $.each(slots, function(index, slot) {
+                        if (!addedTimeSlots.includes(slot)) {
+                            checkSlot(slot)
+                            $('#shedules_time').append('<option value="' + value
+                                .id + '">' + slot + '</option>');
+                            addedTimeSlots.push(slot); 
+                        }
+                    });
                 });
 
                 var selectedStateId = $('#selected_shedules_time').val();
-
                 $('#shedules_time').val(selectedStateId);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX Error:', textStatus, errorThrown);
             }
         });
+    }
+
+    $('#schedule-form').submit(function(e) {
+        e.preventDefault();
+        var selectedTime = $('#shedules_time option:selected').text();
+        var selectedTimePeriod = selectedTime.split(' - ').join(' ');
+        var formData = {
+            option_type: $('input[name="option_type"]:checked').val(),
+            days: $('input[name="days[]"]:checked').map(function() {
+                return this.value;
+            }).get(),
+            selected_time_period: selectedTimePeriod,
+            id: '<?php echo isset($_SESSION['id']) ? $_SESSION['id'] : ''; ?>',
+            Assign_Techer_id: '<?php echo isset($_SESSION['Assign_Techer_id']) ? $_SESSION['Assign_Techer_id'] : ''; ?>'
+        };
+
+    $.ajax({
+        url: '<?= base_url(); ?>selectslotsbystudent',
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        success: function(data) {
+            
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('AJAX Error:', textStatus, errorThrown);
+        }
     });
 });
 
+function checkSlot(slot) {
+    
+        var teacherId = '<?php echo isset($_SESSION['Assign_Techer_id']) ? $_SESSION['Assign_Techer_id'] : ''; ?>';
 
+    $.each(slot, function(index, slot) {
+        $.ajax({
+         url: '<?= base_url(); ?>check_slot_availability',
+            type: 'POST',
+            data: {
+                selectedSlot: slot,
+                studentId: '<?php echo isset($_SESSION['id']) ? $_SESSION['id'] : ''; ?>',
+                 teacherId: teacherId
+             },
+             dataType: 'json',
+             success: function(data) {
+               console.log(slot);
+             },
+             error: function(jqXHR, textStatus, errorThrown) {
+                 console.error('AJAX Error:', textStatus, errorThrown);
+             }
+         })
+     })
+}
 
-
-
+resetDropdownAndFetchData();
+});
 </script>
