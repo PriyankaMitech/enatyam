67dbda5f5c4700f7547c82c2b031841fe8110f2d
rupali21 <rupali21.mitech@gmail.com>
2024-02-06 11:41:26 +0530

    refundPolicy page

--- app/Views/RefundPolicy.php
+++ app/Views/RefundPolicy.php
@@ -1,38 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RefundPolicy</title>
-    <link rel="stylesheet" href="public/css/footer.css">
-    <link rel="stylesheet" href="public/css/RefundPolicy.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.0/css/font-awesome.css" integrity="sha512-CB+XYxRC7cXZqO/8cP3V+ve2+6g6ynOnvJD6p4E4y3+wwkScH9qEOla+BTHzcwB4xKgvWn816Iv0io5l3rAOBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<?php include('header.php'); ?>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <!-- fontawesome icon cdn -->
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" integrity="sha512-Mo79lrQ4UecW8OCcRUZzf0ntfMNgpOFR46Acj2ZtWO8vKhBvD79VCp3VOKSzk6TovLg5evL3Xi3u475Q/jMu4g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- jQuery library -->
-
-</head>
-
-<body>
 
     <section class="contactus">
-
         <div class="container cont-width">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Refund Policy</h1>
-
-
+                    <h1>REFUND POLICY</h1>
                 </div>
-
             </div>
-
         </div>
-
     </section>
     <section class="cont-main-section">
         <div class="container cont-width">
@@ -40,84 +17,94 @@
                 <div class="col-lg-12">
                     <ul class="breadcrumb-ul">
                         <li>
-                            <a href="">Home</a>
+                            <a href="<?php echo base_url(); ?>">Home</a>
                         </li>
                         <li><i class="fa fa-chevron-right" aria-hidden="true"></i>
                         </li>
                         <li>
-                            Refund Policy
+                        Refund Policy
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-    </section>
-    <section class="comming-section ">
-        <div class="row align-items-center">
-            <div class="col-9 text-center offset-1">
-                <p class="comming-soon-p">Our refund policy has been devised to define the situations under which Enatyam will provide a refund, the procedure for claiming a refund and the responsibility of Enatyam in circumstances resulting in such a claim. By registering for any of our services you are declaring that you accept and agree with all the terms and conditions outlined in the refund policy.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Coverage & Scope:</h4>
-                <ul>
-                    <li>Our refund policy covers the refunds by Enatyam and/or a website owned and managed by Enatyam.</li>
-                    <li>This policy does not have any application for companies which are not owned or controlled by Enatyam or for persons not employed or managed by Enatyam. This includes any third party service and / or product providers bound by contract and also, any third-party websites to which Enatyam website link.</li>
+        <div class="container cont-lg">
+            <div class="row termsconp">
+                <div class="col-lg-12">
+                    <h4><b><u>Refund Policy:</u></b></h4>
+                    <p>
+                        Thank you for choosing www.enatyam.com for your online dance, music, and yoga training
+                        needs. We understand that circumstances may arise where a refund is necessary, and we are
+                        committed to providing a hassle-free refund process.
+                    </p>
 
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Filing a Complaint:</h4>
-                <ul>
-                    <li>At Enatyam we take every project with the final product in sight. It is as much our responsibility as the client’s. So, we believe that every effort should be made to reach a solution that is fully acceptable reciprocally in case of any situation where dissatisfaction related to services comes. Only when things are completely out of hand that refund should be considered.</li>
-                    <li>Our final aim is to come to a mutually acceptable solution. Even then if for some reason you are not satisfied and think about going for a refund claim, it is our request that you take out a few precious minutes to write to us at contact@www.enatyam.org for a final dialogue prior to requesting a refund.</li>
-                    <li>Only if we are unable to reach a common ground with you after talks should a refund claim be filed.</li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Eligibility for Refund:</h4>
-                <ul>
-                    <p>At enatyam every project is important to us and we make sure that we handle each project with utmost care and professionalism. Our aim is to provide the results as per the terms and conditions of the proposal. However, despite every measure if the client is not satisfied with the results and chooses to go for the dispute resolution process, we consider refund requests as per the following program in which our services and refund policy related to each service is very clearly listed.</p>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Delivery Time:</h4>
-                <ul>
-                    <li>Full refund: In cases where the project has not been started or if the initial design style has not been approved. The Full Refund policy will initiate after receiving the upfront payment date and will take 7 working days.</li>
-                    <li>Partial refund: If there is a failure to deliver as per our delivery policy after the approval of the initial design style. The partial refund will be in proportion to the work completed.</li>
-                    <li>No refunds: If the project has been completed and uploaded on the server.</li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Processing of Refunds:</h4>
-                <ul>
-                    <p>The refunds will be processed and mailed within 7 business days of the date of cancellation and will be brought about using the method of payment agreed upon in the beginning of the project ie; refund by check or refund by credit card.</p>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h4>Contacting us regarding our refund policy:</h4>
-                <ul>
-                    <p>If you have queries or suggestions regarding our refund policies kindly e–mail us at contact@www.enatyam.org.</p>
-                </ul>
+                    <ol class="list-group list-group-numbered">
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold">  No Questions Asked Refund Policy:</div>
+                                At enatyam, we operate under a "No Questions Asked" refund policy. If you are dissatisfied
+                                with our online training services for any reason, you are entitled to a refund within a specified
+                                refund period.. No Questions Asked Refund Policy:
+                                At enatyam, we operate under a "No Questions Asked" refund policy. If you are dissatisfied
+                                with our online training services for any reason, you are entitled to a refund within a specified
+                                refund period.
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold">Refund Period:</div>
+                                The refund period starts from the date of your purchase and extends for 15 working days. You
+                                must request a refund within this period to be eligible.
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"> How to Request a Refund:</div>
+                                To initiate a refund, please send an email to hello@enatyam.com with your name, the course
+                                or service you are requesting a refund for, and the reason for your request. Our customer
+                                support team will process your refund promptly.
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold">Processing Time:</div>
+                                Refunds will be processed within 7 days of receiving your refund request. Please note that it
+                                may take additional time for the refund to reflect in your account, depending on your
+                                payment method.
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"> Exceptions:</div>
+                                While our goal is to provide a fair and transparent refund policy, there are certain situations
+                                where refunds may not be applicable. These include cases where a user has violated our
+                                terms and conditions or engaged in fraudulent activities.
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"> Contact Information:</div>                       
+                                If you have any questions or concerns regarding our refund policy, please contact our
+                                customer support team at hello@enatyam.com
+                            </div>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"> Changes to Refund Policy:</div>
+                                We reserve the right to modify or update our refund policy at any time. Any changes will be
+                                effective immediately and will be communicated through our website.
+                                Thank you for choosing [Your Website Name]. We value your trust and strive to provide you
+                                with the best online training experience.<br><br>
+                                <b>Effective Date: </b>31.01.2024
+                            </div>
+                        </li>   
+                    </ol>
+                </div>
             </div>
         </div>
     </section>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-</body>
+  
+<hr>
 <?php include('footer.php'); ?>
 
-</html>
+</html> 
