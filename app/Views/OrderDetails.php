97ebc4f3d37e85792a77c8a9b1f324c3e15f1f21
PriyankaMitech <priyanka.mitech@gmail.com>
2024-02-09 15:36:51 +0530

    some changes

--- app/Views/OrderDetails.php
+++ app/Views/OrderDetails.php
@@ -271,14 +271,8 @@ $return_url = site_url().'PaymentController/payment';
     </div>
 </div>
 
-<div class="row m-0">
-    <div class="col-md-6 col-12 text-end mb-2">
-        <a href="<?php echo base_url('demo'); ?>" class="btnnew btn-book text-center byfd">Book your Free demo now</a>
-    </div>
-    <div class="col-md-6 col-12 text-star">
-        <a href="<?php echo base_url('Contactus'); ?>" class="btnnew btn-request text-center nsracb">Not sure ?? Request a call back</a>
-    </div>
-</div>
+                        </div>
+                        <?php include('bookdemocallb.php'); ?>
 
 <?php include('footer.php'); ?>
 
