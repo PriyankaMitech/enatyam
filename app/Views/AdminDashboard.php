21dd4566d7ac8d0cbe450d08ae593f9ef890bd2b
PriyankaMitech <priyanka.mitech@gmail.com>
2024-04-25 17:42:36 +0530

    Normal Changes

--- app/Views/AdminDashboard.php
+++ app/Views/AdminDashboard.php
@@ -569,13 +569,13 @@ th {
                                                     <select name="faculty_id">
                                                         <option value="" selected>Select Faculty</option>
                                                         <?php foreach ($Faculty as $facultyItems) : ?>
-                                                        <?php if ($facultyItems->course == $facult->course && $facultyItems->sub_course == $facult->sub_course) : ?>
+                                                        <?php //if ($facultyItems->course == $facult->course && $facultyItems->sub_course == $facult->sub_course) : ?>
                                                         <option value="<?= $facultyItems->id ?>" <?php if ($facult->AssignTecher_id == $facultyItems->id) {
                                                                                                     echo "selected";
                                                                                                 } ?>>
                                                             <?= $facultyItems->full_name ?>
                                                         </option>
-                                                        <?php endif; ?>
+                                                        <?php //endif; ?>
                                                         <?php endforeach; ?>
                                                     </select>
                                                 </td>
