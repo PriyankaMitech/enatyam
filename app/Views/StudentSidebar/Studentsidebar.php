3b3e2af77f83153fd0be41deb63c49a0148886f9
PriyankaMitech <priyanka.mitech@gmail.com>
2024-06-10 15:35:08 +0530

    Error Slove Of Front Pages

--- app/Views/StudentSidebar/Studentsidebar.php
+++ app/Views/StudentSidebar/Studentsidebar.php
@@ -21,7 +21,9 @@
 
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script> -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 
   <style>
 
